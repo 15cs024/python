b0c5939945bab4fd03266ada223fc9bffad6aa63
15cs024 <36753056+15cs024@users.noreply.github.com>
2018-02-23 16:00:04 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,9 @@
+ r1=raw_input("value is:")
+    r2=raw_input("value is:")
+    r3=raw_input("value is:")
+    if(r1>r2):
+        print("r1 is greater")
+    elif(r2>r3):
+        print("r2 is greater")
+    elif(r3>r1):
+        print("r3 is greater")
