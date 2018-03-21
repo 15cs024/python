3933e6c7f2295f00760820c4c52bdca4d60e293e
15cs024 <36753056+15cs024@users.noreply.github.com>
2018-03-21 16:15:34 +0530

    Update strong.py

--- strong.py
+++ strong.py
@@ -1,11 +1,11 @@
-lower = int(input("Enter lower range: "))
-upper = int(input("Enter upper range: "))
-for num in range(lower,upper + 1):
-    sum = 0
+lower = int(input("lower range: "))
+upper = int(input("upper range: "))
+for num in range(lower,upper + 2):
+    sum=1
 temp = num
 while temp > 0:
  digit = temp % 10
-sum += digit ** 3
+sum += digit ** 2
 temp //= 10
 if num == sum:
-     print(num)
+     print(num is)
